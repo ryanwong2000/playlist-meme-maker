58811db3f9d000a7fbed3419d83a07949adfd51b
Ryan Wong <56795999+ryanwong2000@users.noreply.github.com>
2022-02-27 09:36:49 -0500

    make login button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Login } from './components/Login';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Spotify Playlist Meme Maker🎼</h1>
+      <Login />
     </div>
   );
 }
---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <button><a href='/auth'>Login</a></button>
+  )
+}
