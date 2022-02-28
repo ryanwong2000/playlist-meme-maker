ca35d2201b8a11038400f1da8d1e247a34cd8015
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-02-27 22:46:38 -0500

    refresh access token

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+/**
+ * We use the server to retrieve access and refresh tokens and server them back to the client
+ * We use axios on the frontend to get to our routes/api on the backend
+ */
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config();
@@ -34,4 +38,27 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.listen(port);
+app.post('/refresh', async (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: redirectUri,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken
+  });
+
+  try {
+    data = await spotifyApi.refreshAccessToken();
+    res.json({
+      accessToken: data.body.accessToken,
+      expiresIn: data.body.expiresIn
+    });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
+});
+
+app.listen(port, () =>
+  console.log(`HTTP Server up. Now go to ${port} in your browser.`)
+);
