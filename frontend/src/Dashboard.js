91d3ce6243b47f270eb8511472027d146a0080d3
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-03-02 18:03:36 -0500

    returns for entire search query

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -30,11 +30,12 @@ export const Dashboard = ({ code }) => {
     if (!search) return setSearchResults([]);
 
     let cancel = false;
+    let results = [];
 
-    spotifyApi.searchTracks(search, { limit: 3 }).then((data) => {
+    spotifyApi.searchTracks(search).then((res) => {
       if (cancel) return;
       setSearchResults(
-        data.body.tracks.items.map((track) => {
+        res.body.tracks.items.map((track) => {
           const smallestAlbumImage = track.album.images.reduce(
             (smallest, image) => {
               if (image.height < smallest.height) return image;
